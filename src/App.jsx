263e80b3e338880116ad405f9be255c568db1640
Niklas Pelli <niklaspelli22@gmail.com>
2026-04-12 22:51:42 +0200

    waves background added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ContactForm from "./components/Contactform/ContactForm";
 import { useState } from "react";
 import { UserProvider } from "./context/Usercontext.jsx";
 
-import "./styles.css";
+import "./waves.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import WelcomeOverlay from "./components/WelcomeOverlay.jsx";
@@ -25,25 +25,28 @@ function App() {
   return (
     <UserProvider>
       <Router>
+        {/* --- BAKGRUNDS-VÅGOR --- */}
+        <div className="wave-container">
+          <div className="wave grey-wave"></div>
+          <div className="wave white-wave"></div>
+        </div>
+
+        {/* --- NAVIGATION --- */}
         <Navbar />
+
+        {/* --- LOGIN/WELCOME OVERLAY --- */}
+        {/* Denna renderas nu globalt så den täcker hela sidan tills man är klar */}
+        <WelcomeOverlay />
 
         <div className="container">
           <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <About name={userName} />
-                  <WelcomeOverlay />
-                  {/*  <LandingPage /> */}
-                </>
-              }
-            />
-            <Route path="/About" element={<About name={userName} />} />
+            <Route path="/" element={<About />} />
+            <Route path="/About" element={<About />} />
             <Route path="/Work" element={<Work />} />
             <Route path="/Contact" element={<ContactForm />} />
           </Routes>
         </div>
+
         <Footer />
       </Router>
     </UserProvider>
